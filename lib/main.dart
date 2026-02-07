32b04b4054280139c07e84d27b8fc0acf62bb554
hafzdwlar097-ux <hafzdwlar097@gmail.com>
2026-02-07 08:33:28 +0200

    stable_build

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,37 @@
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(home: ColorFlashlight()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class ColorFlashlight extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+  _ColorFlashlightState createState() => _ColorFlashlightState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class _ColorFlashlightState extends State<ColorFlashlight> {
+  Color _bgColor = Colors.white;
+  String _colorName = "White";
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Color _backgroundColor = Colors.white;
-  Color _colorName = Colors.black;
-  String _colorText = 'WHITE';
-  bool _showResetIcon = false;
-
-  void _changeBackgroundColor() {
+  void _changeColor() {
     setState(() {
-      _backgroundColor = Color.fromRGBO(
-        _randomNumber(255),
-        _randomNumber(255),
-        _randomNumber(255),
-        1,
-      );
-      _colorName = _getTextColor(_backgroundColor);
-      _colorText = _getEnglishColorName(_backgroundColor);
+      _bgColor = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+      _colorName = "Color: #${_bgColor.value.toRadixString(16).toUpperCase().substring(2)}";
     });
-  }
-
-  void _showResetIconOnLongPress() {
-    setState(() {
-      _showResetIcon = true;
-    });
-  }
-
-  void _resetBackgroundColor() {
-    setState(() {
-      _backgroundColor = Colors.white;
-      _colorName = Colors.black;
-      _colorText = 'WHITE';
-      _showResetIcon = false;
-    });
-  }
-
-  int _randomNumber(int max) {
-    return Random().nextInt(max);
-  }
-
-  Color _getTextColor(Color backgroundColor) {
-    return ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.light
-        ? Colors.black
-        : Colors.white;
-  }
-
-  String _getEnglishColorName(Color color) {
-    if (color == Colors.white) return 'WHITE';
-    if (color == Colors.black) return 'BLACK';
-    if (color == Colors.red) return 'RED';
-    if (color == Colors.green) return 'GREEN';
-    if (color == Colors.blue) return 'BLUE';
-    if (color == Colors.yellow) return 'YELLOW';
-    if (color == Colors.purple) return 'PURPLE';
-    if (color == Colors.orange) return 'ORANGE';
-    if (color == Colors.pink) return 'PINK';
-    if (color == Colors.brown) return 'BROWN';
-    if (color == Colors.grey) return 'GREY';
-    return 'CUSTOM';
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _changeBackgroundColor,
-      onLongPress: _showResetIconOnLongPress,
-      child: Container(
-        color: _backgroundColor,
-        child: Stack(
-          children: [
-            Center(
-              child: Text(
-                _colorText,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: _colorName,
-                ),
-              ),
-            ),
-            if (_showResetIcon)
-              Positioned(
-                top: 20,
-                right: 20,
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.reset_tv,
-                    color: Colors.white,
-                  ),
-                  onPressed: _resetBackgroundColor,
-                ),
-              ),
-          ],
+    return Scaffold(
+      backgroundColor: _bgColor,
+      body: GestureDetector(
+        onTap: _changeColor,
+        onLongPress: () => setState(() { _bgColor = Colors.white; _colorName = "White"; }),
+        child: Center(
+          child: Text(
+            _colorName,
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black54),
+          ),
         ),
       ),
     );
