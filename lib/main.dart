4ffb25e765b6f25adc48d17d2d58a2c05676cb4f
hafzdwlar097-ux <hafzdwlar097@gmail.com>
2026-02-07 16:54:22 +0200

    Final Build Fix

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      title: 'Setup',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter App'),
+        title: const Text('Setup'),
       ),
       body: const Center(
-        child: Text('Hello, World!'),
+        child: Text('Setup'),
       ),
     );
   }
