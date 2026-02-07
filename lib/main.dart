29fe648948fa46ded4a401842d24272d1ed53adb
hafzdwlar097-ux <hafzdwlar097@gmail.com>
2026-02-07 19:41:24 +0200

    AI_Build_mmdddz

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CalculatorApp());
+  runApp(const MyApp());
 }
 
-class CalculatorApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'آلة حاسبة',
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: CalculatorHome(),
-    );
-  }
-}
-
-class CalculatorHome extends StatefulWidget {
-  @override
-  _CalculatorHomeState createState() => _CalculatorHomeState();
-}
-
-class _CalculatorHomeState extends State<CalculatorHome> {
-  String output = "0";
-  String _output = "0";
-  double num1 = 0.0;
-  double num2 = 0.0;
-  String operand = "";
-
-  buttonPressed(String buttonText) {
-    if (buttonText == "CLEAR") {
-      _output = "0";
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else if (buttonText == "+"  buttonText == "-"  buttonText == "/" || buttonText == "X") {
-      num1 = double.parse(output);
-      operand = buttonText;
-      _output = "0";
-    } else if (buttonText == ".") {
-      if (_output.contains(".")) {
-        return;
-      } else {
-        _output = _output + buttonText;
-      }
-    } else if (buttonText == "=") {
-      num2 = double.parse(output);
-      if (operand == "+") {
-        _output = (num1 + num2).toString();
-      }
-      if (operand == "-") {
-        _output = (num1 - num2).toString();
-      }
-      if (operand == "X") {
-        _output = (num1 * num2).toString();
-      }
-      if (operand == "/") {
-        _output = (num1 / num2).toString();
-      }
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else {
-      _output = _output + buttonText;
-    }
-    setState(() {
-      output = double.parse(_output).toStringAsFixed(2).replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "");
-    });
-  }
-
-  Widget buildButton(String buttonText) {
-    return Expanded(
-      child: OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          padding: EdgeInsets.all(24.0),
-        ),
-        child: Text(buttonText, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-        onPressed: () => buttonPressed(buttonText),
+      title: 'التطبيق',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
   }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("آلة حاسبة")),
-      body: Container(
+      appBar: AppBar(
+        title: const Text('الصفحة الرئيسية'),
+      ),
+      body: Center(
         child: Column(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
-              child: Text(output, style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold))),
-            Expanded(child: Divider()),
-            Column(children: [
-              Row(children: [buildButton("7"), buildButton("8"), buildButton("9"), buildButton("/")]),
-              Row(children: [buildButton("4"), buildButton("5"), buildButton("6"), buildButton("X")]),
-              Row(children: [buildButton("1"), buildButton("2"), buildButton("3"), buildButton("-")]),
-              Row(children: [buildButton("."), buildButton("0"), buildButton("00"), buildButton("+")]),
-              Row(children: [buildButton("CLEAR"), buildButton("=")]),
-            ])
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('مرحباً بالزائر'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondPage()),
+                );
+              },
+              child: const Text('انتقل إلى الصفحة الثانية'),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('الصفحة الثانية'),
+      ),
+      body: const Center(
+        child: Text('لقد انتقلت إلى الصفحة الثانية'),
       ),
     );
   }
