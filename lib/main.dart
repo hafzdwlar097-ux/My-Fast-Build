69c12e182e7fdc123007f92a78bd264d6ec548de
hafzdwlar097-ux <hafzdwlar097@gmail.com>
2026-02-07 19:21:54 +0200

    Force Build wkwn

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 
 import 'package:flutter/material.dart';
-// Build ID: mekf
+// Build ID: wkwn
 // هذا الكود مختلف تماماً عن العداد
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar(title: Text('FastBridge mekf')),
+    appBar: AppBar(title: Text('FastBridge wkwn')),
     body: Center(child: Icon(Icons.send, size: 100, color: Colors.blue)),
   ),
 ));
