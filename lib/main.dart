0d8c99d3c7ef45eb4d4e97c776da76ef9952d6f5
hafzdwlar097-ux <hafzdwlar097@gmail.com>
2026-02-07 08:36:43 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,40 @@
-
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() => runApp(MaterialApp(home: ColorFlashlight()));
-
-class ColorFlashlight extends StatefulWidget {
-  @override
-  _ColorFlashlightState createState() => _ColorFlashlightState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _ColorFlashlightState extends State<ColorFlashlight> {
-  Color _bgColor = Colors.white;
-  String _colorName = "White";
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  void _changeColor() {
-    setState(() {
-      _bgColor = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
-      _colorName = "Color: #${_bgColor.value.toRadixString(16).toUpperCase().substring(2)}";
-    });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'تطبيق بسيط',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
   }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _bgColor,
-      body: GestureDetector(
-        onTap: _changeColor,
-        onLongPress: () => setState(() { _bgColor = Colors.white; _colorName = "White"; }),
-        child: Center(
-          child: Text(
-            _colorName,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black54),
-          ),
+      appBar: AppBar(
+        title: const Text('تطبيق بسيط'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('مرحباً بكم في التطبيق البسيط'),
+            Text('هذا تطبيق بسيط جداً'),
+          ],
         ),
       ),
     );
